826c64ca10328c98dc29756b012a85ddd8a1b0b6
az0o0z3949-droid <az0o0z3949@gmail.com>
2026-09-21 12:31:57 +0300

    Update sx1280_jammer.h

--- src/modules/SX1280/sx1280_jammer.h
+++ src/modules/SX1280/sx1280_jammer.h
@@ -1,7 +1,14 @@
 #ifndef SX1280_JAMMER_H
 #define SX1280_JAMMER_H
 #include <Arduino.h>
-enum SX1280JamMode { JAM_CW=0, JAM_DATA_FLOOD=1, JAM_CHANNEL_HOP=2 };
+
+enum SX1280JamMode {
+    JAM_DATA_FLOOD  = 0,
+    JAM_CHANNEL_HOP = 1,
+    JAM_CW          = 2
+};
+
 void sx1280_jammer_menu();
-void sx1280_jammer_run(int mode, float startFreq, float endFreq);
+void sx1280_jammer_run(int mode, float startFreq, float endFreq, const char* presetName);
+
 #endif
